d98111a91c693a40932cb295ba71dda15b8f3776
candracandra1525 <candraandi2003.gmail.com>
2022-10-25 22:20:13 +0700

    Aplikasi Quotes 25/10/2022

---
+++ app/src/main/java/com/mdp/aplikasiquotes/Model/QuoteModel.java
@@ -0,0 +1,17 @@
+package com.mdp.aplikasiquotes.Model;
+
+public class QuoteModel
+{
+    private String text, author;
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+}
